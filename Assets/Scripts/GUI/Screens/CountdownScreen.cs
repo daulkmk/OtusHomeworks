68ee8041aa2e66e6cefac809eafe161d69ecde08
Dmitry Putilin <putilin.dmitry@bk.ru>
2024-03-21 19:21:59 +0700

    In progress

---
+++ Assets/Scripts/GUI/Screens/CountdownScreen.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShootEmUp.UI
+{
+    public class CountdownScreen : MonoBehaviour
+    {
+        [SerializeField] private Text _counterTxt;
+
+        private void OnEnable()
+        {
+            _counterTxt.text = "";
+        }
+
+        public void Show(int time, Action onCountComplete)
+        {
+            gameObject.SetActive(true);
+            StartCoroutine(Countdown(time, onCountComplete));
+        }
+
+        public void Hide() => gameObject.SetActive(false);
+
+        private IEnumerator Countdown(int time, Action onCountComplete)
+        {
+            var yield = new WaitForSecondsRealtime(1);
+            for (; time > 0; time--)
+            {
+                SetCount(time);
+                yield return yield;
+            }
+            onCountComplete?.Invoke();
+        }
+
+        private void SetCount(int count) => _counterTxt.text = count.ToString();
+    }
+}
---
+++ Assets/Scripts/GUI/Screens/SingleButtonScreen.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ShootEmUp.UI
+{
+    public class SingleButtonScreen : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        private Action onActionRequest;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+        public void Show(Action onActionRequested)
+        {
+            onActionRequest = onActionRequested;
+            gameObject.SetActive(true);
+        }
+
+        public void Hide() => gameObject.SetActive(false);
+
+        private void OnButtonClick()
+        {
+            onActionRequest?.Invoke();
+        }
+    }
+}
