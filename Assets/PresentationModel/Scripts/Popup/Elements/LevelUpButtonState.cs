ec0600f180d832a2d6348c2234f2df5ad0c6091a
Dmitry Putilin <putilin.dmitry@bk.ru>
2024-05-14 21:30:32 +0700

    import homework materials

--- Assets/PresentationModel/Scripts/Popup/Elements/LevelUpButtonState.cs
+++
@@ -1,9 +0,0 @@
-namespace ShootEmUp.PresentationModel
-{
-    public enum LevelUpButtonState
-    {
-        None       = 0,
-        Available = 1,
-        Locked    = 2,
-    }
-}
--- Assets/PresentationModel/Scripts/Popup/IPopup.cs
+++
@@ -1,8 +0,0 @@
-namespace ShootEmUp.PresentationModel
-{
-    internal interface IPopup
-    {
-        void Show(IPopupPresenter presenter);
-        void Hide();
-    }
-}
